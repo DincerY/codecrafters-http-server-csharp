1bcd23579d8d8e92a3735651c092a853fd465d5f
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-07-02 17:17:22 +0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -2,8 +2,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-Console.WriteLine("Logs from your program will appear here!");
-
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
