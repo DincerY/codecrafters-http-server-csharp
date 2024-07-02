e5ade4bf935e68738aa80eb7c101db8f0a5560cc
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-07-02 16:17:36 +0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -10,7 +10,17 @@ TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
 
+var responseBuffer = new byte[1024];
+int receivedBytes = socket.Receive(responseBuffer);
+
+var val = Encoding.UTF8.GetString(responseBuffer).Split("\r\n");
 
 
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+var lineFirstPart = val[0].Split(" ");
+
+var (method, path, httpVersion) = (lineFirstPart[0], lineFirstPart[1], lineFirstPart[2]);
+
+var response = path == "/" ? $"{httpVersion} 200 OK\r\n\r\n" : $"{httpVersion} 404 Not Found \r\n\r\n";
+
+socket.Send(Encoding.UTF8.GetBytes(response));
 
