d4bf682d2b5ca9ce3e8547db7a8cbd935b3c3dcd
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-07-02 16:24:29 +0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -18,7 +18,9 @@ var val = Encoding.UTF8.GetString(responseBuffer).Split("\r\n");
 
 var lineFirstPart = val[0].Split(" ");
 
-var (method, path, httpVersion) = (lineFirstPart[0], lineFirstPart[1], lineFirstPart[2]);
+var method = lineFirstPart[0];
+var path = lineFirstPart[1];
+var httpVersion = lineFirstPart[2];
 
 var response = path == "/" ? $"{httpVersion} 200 OK\r\n\r\n" : $"{httpVersion} 404 Not Found \r\n\r\n";
 
