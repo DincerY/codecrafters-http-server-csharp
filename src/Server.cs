0bf7a545611d81ca85495546c497b9840f2ebbe0
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-07-03 16:06:47 +0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -10,6 +10,8 @@ using System.Text;
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 
+
+
 while (true)
 {
     var socket = server.AcceptSocket();
@@ -35,7 +37,16 @@ Task HandleConnection(Socket socket)
     else if (request.Path.StartsWith("/echo/"))
     {
         var message = request.Path.Substring(6);
-        response = $"{request.HttpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {message.Length}\r\n\r\n{message}";
+        if (request.Headers.ContainsValue("gzip"))
+        {
+            response = new Response(request.HttpVersion, StatusCode.Ok, message, "text/plain", "gzip").ToString();
+        }
+        else
+        {
+            //response = $"{request.HttpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {message.Length}\r\n\r\n{message}";
+            response = new Response(request.HttpVersion, StatusCode.Ok, message, "text/plain").ToString();
+        }
+        
     }
     else if (request.Path.StartsWith("/user-agent"))
     {
@@ -96,22 +107,35 @@ class Response
         Body = body;
         ContentType = contentType;
     }
+    public Response(string version, StatusCode status, string body, string contentType, string encoding) : this(version, status,body, contentType)
+    {
+        Encoding = encoding;
+    }
     public StatusCode Status { get; }
     public string Body { get; }
     public string? ContentType { get; }
     public string? Version { get; }
+    public string Encoding { get; set; }
 
     public override string ToString()
     {
-        //return $"{Version} {(int)Status} {Status.GetDescription()}\r\nContent-Type: application/octet-stream\r\nContent-Length: {fileText.Length}\r\n\r\n{fileText}";
         StringBuilder builder = new StringBuilder();
         builder.Append($"{Version} {(int)Status} {Status.GetDescription()}\r\n");
         if (ContentType != null)
         {
-            builder.Append($"Content-Type: {ContentType}\r\nContent-Length: {Body.Length}\r\n\r\n{Body}");
+            builder.Append($"Content-Type: {ContentType}\r\nContent-Length: {Body.Length}");
+        }
+
+        if (Encoding == "gzip")
+        {
+            builder.Append($"Context-Encoding: {Encoding}\r\n");
+        }
+
+        if (Body != null)
+        {
+            builder.Append($"\r\n\r\n{Body}");
         }
         return builder.ToString();
-        
     }
 }
 
@@ -124,8 +148,17 @@ class Request
         Path = Lines[0].Split(" ")[1];
         HttpVersion = Lines[0].Split(" ")[2];
         Body = Lines[^1].TrimEnd();
-    }
+        Headers = new Dictionary<string, string>();
 
+        for (int i = 1; i < Lines.Length - 2; i++)
+        {
+            string[] content = Lines[i].Trim().Split(": ");
+            string key = content[0];
+            string value = content[1];
+            Headers.Add(key, value);
+        }
+    }
+    public Dictionary<string, string> Headers { get; private set; }
     public string[] Lines { get; }
     public string HttpMethod { get; }
     public string Path { get; }
@@ -147,7 +180,6 @@ enum StatusCode
     [Description("Internal Server Error")]
     InternalServerError = 500,
 }
-
 
 public static class EnumExtensions
 {
