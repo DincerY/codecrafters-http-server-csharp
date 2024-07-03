5ef98886e18e0fb8f3064a3305f98af0ce8dfa64
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-07-03 13:12:23 +0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -18,51 +18,46 @@ Task HandleConnection(Socket socket)
     var responseBuffer = new byte[1024];
     socket.Receive(responseBuffer);
 
-    var lines = Encoding.UTF8.GetString(responseBuffer).Split("\r\n");
 
-
-    var lineFirstPart = lines[0].Split(" ");
-    var method = lineFirstPart[0];
-    var path = lineFirstPart[1];
-    var httpVersion = lineFirstPart[2];
+    Request request = new Request(responseBuffer);
 
     string response = "";
 
-    if (path == "/")
+    if (request.Path == "/")
     {
-        response = $"{httpVersion} 200 OK\r\n\r\n";
+        response = $"{request.HttpVersion} {HttpStatus.OK} OK\r\n\r\n";
     }
-    else if (path.StartsWith("/echo/"))
+    else if (request.Path.StartsWith("/echo/"))
     {
-        var message = path.Substring(6);
-        response = $"{httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {message.Length}\r\n\r\n{message}";
+        var message = request.Path.Substring(6);
+        response = $"{request.HttpVersion} {HttpStatus.OK} OK\r\nContent-Type: text/plain\r\nContent-Length: {message.Length}\r\n\r\n{message}";
     }
-    else if (path.StartsWith("/user-agent"))
+    else if (request.Path.StartsWith("/user-agent"))
     {
-        var userAgent = lines.SingleOrDefault(a => a.Contains("User-Agent:"));
+        var userAgent = request.Lines.SingleOrDefault(a => a.Contains("User-Agent:"));
         var headerVal = userAgent.Split(": ")[1];
-        response = $"{httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {headerVal.Length}\r\n\r\n{headerVal}";
+        response = $"{request.HttpVersion} {HttpStatus.OK} OK\r\nContent-Type: text/plain\r\nContent-Length: {headerVal.Length}\r\n\r\n{headerVal}";
     }
-    else if (path.StartsWith("/files/"))
+    else if (request.Path.StartsWith("/files/"))
     {
-        var fileName = path.Substring(7);
+        var fileName = request.Path.Substring(7);
         string fileText = "";
         var directory = Environment.GetCommandLineArgs()[2];
         string filePath = $"{directory}/{fileName}";
         if (File.Exists(filePath))
         {
             fileText = File.ReadAllText(filePath);
-            response = $"{httpVersion} 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {fileText.Length}\r\n\r\n{fileText}";
+            response = $"{request.HttpVersion} {HttpStatus.OK} OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {fileText.Length}\r\n\r\n{fileText}";
         }
         else
         {
-            response = $"{httpVersion} 404 Not Found\r\n\r\n";
+            response = $"{request.HttpVersion} {HttpStatus.NotFound} Not Found\r\n\r\n";
         }
-       
+
     }
     else
     {
-        response = $"{httpVersion} 404 Not Found\r\n\r\n";
+        response = $"{request.HttpVersion} {HttpStatus.NotFound} Not Found\r\n\r\n";
     }
 
     socket.Send(Encoding.UTF8.GetBytes(response));
@@ -70,6 +65,27 @@ Task HandleConnection(Socket socket)
     return Task.CompletedTask;
 }
 
+class Request
+{
+    public Request(byte[] buffer)
+    {
+        Lines = Encoding.UTF8.GetString(buffer).Split("\r\n");
+        Method = Lines[0].Split(" ")[0];
+        Path = Lines[0].Split(" ")[1];
+        HttpVersion = Lines[0].Split(" ")[2];
+    }
+
+    public string[] Lines { get; }
+    public string Method { get; }
+    public string Path { get; }
+    public string HttpVersion { get; }
+}
+
+enum HttpStatus
+{
+    OK = 200,
+    NotFound = 404
+}
 
 
 
